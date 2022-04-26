b37b01762555d6a1c7798bd87d9daa7351f7decf
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-26 15:42:12 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/bookstore/api/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.bookstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
